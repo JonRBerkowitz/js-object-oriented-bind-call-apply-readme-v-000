02995523be94828721cf3265482694ac8716297d
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-02-13 19:48:20 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,8 +1,26 @@
-let sally = { name: 'Sally' };
-
-function greet(customerOne, customerTwo) {
-    console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}!`);
+class Event {
+    constructor(title, keywords) {
+        this.title = title;
+        this.keywords = keywords;
+    }
 }
 
-greet.call(sally, 'Terry', 'George');
-// Hi Terry and George, my name is Sally!
+class User {
+    constructor(name, interests) {
+        this.name = name;
+        this.interests = interests;
+    }
+
+    matchInterests(event) {
+        return event.keywords.some(
+            function(word) {
+                return this.interests.includes(word);
+            }.bind(this) // added to the and of the callback function
+        );
+    }
+}
+
+let billy = new User('billy', ['music', 'art', 'movies']);
+let freeMusic = new Event('Free Music Show', ['music', 'free', 'outside']);
+
+billy.matchInterests(freeMusic);
